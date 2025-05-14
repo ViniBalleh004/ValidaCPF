1b27a2f0a8259c4aff855909111562430b4baa10
gdf06 <guilhermedf563@hotmail.com>
2025-05-13 22:14:13 -0300

    validação do ultimo digito concluida, como algumas correção no html e css

--- cpf.js
+++ cpf.js
@@ -18,4 +18,41 @@ function validarCPF(cpf) {
     if(resto === 10 || resto === 11){
         resto = 0;
     }
+
+    if(resto !== parseInt(cpf.substring(9, 10))){
+        return false;
+    }
+
+    // Validação do 2º DV //
+    soma= 0;
+    for(let i = 1; i <=10; i++){
+        soma += parseInt(cpf.substring(i -1, i)) * (12 - i);
+    }
+    resto = (soma*10) % 11;
+
+    if(resto === 10 || resto === 11){
+        resto = 0;
+    }
+
+    if(resto !== parseInt(cpf.substring(10,11))){
+        return false;
+    }
+
+    return true;
 }
+
+document.getElementById("cpfForm").addEventListener("submit", function(e){
+    e.preventDefault();
+    const cpfInput = document.getElementById("cpf").value;
+    const messageDiv = document.getElementById("message");
+
+    if(validarCPF(cpfInput)){
+        messageDiv.textContent = "CPF Válido";
+        messageDiv.className = "message success";
+    } else{
+        messageDiv.textContent = "CPF Inválido"
+        messageDiv.className = "message error";
+    }
+
+    messageDiv.style.display = "block";
+});
